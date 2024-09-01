7236303f7eed8120f6c082bc598cbe7edb75a8b0
abdullahsn10 <1201952@student.birzeit.edu>
2024-09-01 12:26:57 +0300

    Alembic setup and DB migrations

---
+++ src/models/notification.py
@@ -0,0 +1,13 @@
+from src.settings.database import Base
+from sqlalchemy import Column, Integer, String, DateTime
+
+
+class Notification(Base):
+    __tablename__ = "notification"
+
+    id = Column(Integer, primary_key=True, index=True)
+    order_id = Column(Integer, nullable=False, index=True)
+    issuer_id = Column(Integer, nullable=False, index=True)
+    customer_id = Column(Integer, nullable=False, index=True)
+    message = Column(String, nullable=False)
+    created_at = Column(DateTime, nullable=False)
