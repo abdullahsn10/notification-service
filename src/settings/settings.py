211949c2bf1811ed41936acd6de22ea057eb91da
Abdullah S. Naser <121259956+abdullahsn10@users.noreply.github.com>
2024-10-30 15:26:37 +0200

    Containerization: Build the app image (#5)
    * Setup Dockerfile and deployments configs
    
    * Update RabbitMQ configs
    
    * Add OpenAPI configs as env var
    
    ---------
    
    Co-authored-by: Abdullah Naser <Abdullah.Naser@exalt.ps>

--- src/settings/settings.py
+++ src/settings/settings.py
@@ -5,14 +5,24 @@ from dotenv import load_dotenv
 # load environment variables
 load_dotenv()
 
+# load database settings
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+DB_SERVICE = os.getenv("DB_SERVICE")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+
+# database url
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{DB_SERVICE}/{POSTGRES_DB}"
+)
+
 # database settings
 DATABASE_SETTINGS = {
-    "URL": os.getenv("SQLALCHEMY_DATABASE_URL"),
+    "URL": SQLALCHEMY_DATABASE_URL,
 }
 
 # security settings
-with open(os.getenv("PUBLIC_KEY_PATH"), "r") as key_file:
-    PUBLIC_KEY = key_file.read()
+PUBLIC_KEY = os.getenv("PUBLIC_KEY")
 
 
 JWT_TOKEN_SETTINGS = {
@@ -24,4 +34,11 @@ JWT_TOKEN_SETTINGS = {
 
 # RabbitMQ and Order-Notification settings
 RABBITMQ_HOST = os.getenv("RABBITMQ_HOST")
+RABBITMQ_USER = os.getenv("RABBITMQ_USER")
+RABBITMQ_PASSWORD = os.getenv("RABBITMQ_PASSWORD")
 ORDER_NOTIFICATION_QUEUE = os.getenv("ORDER_NOTIFICATION_QUEUE")
+
+
+# docs settings
+OPENAPI_URL = os.getenv("OPENAPI_URL")
+ROOT_PATH = os.getenv("ROOT_PATH")
