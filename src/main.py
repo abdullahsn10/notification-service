c561e857b22011b16d14d47164ef8a6d73c2c451
Abdullah Naser <Abdullah.Naser@exalt.ps>
2024-10-30 15:20:43 +0200

    Add OpenAPI configs as env var

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 from src.routers import notification
+from src.settings.settings import OPENAPI_URL, ROOT_PATH
 
-app = FastAPI()
+app = FastAPI(
+    openapi_url=OPENAPI_URL,
+    root_path=ROOT_PATH,
+)
+
 
 # register routes
 app.include_router(notification.router)
